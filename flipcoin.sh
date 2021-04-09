6e128b65e691e7213a0e522d4cc34521bf31b449
pinkyreddy10 <prashureddy97@gmail.com>
2021-04-09 12:18:57 +0530

    [Prasanthi] changed flipcoin.sh file

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-result=$(( RANDOM%2 ))
-echo $result
-if [ $result -eq 0 ]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+heads=0
+tails=0
+for (( i=1; i<=30; i++ ))
+do
+	result=$(( RANDOM%2 ))
+	echo $result
+	if [ $result -eq 0 ]
+	then
+		heads=$(( heads+1 ))
+	else
+		tails=$(( tails+1 ))
+	fi
+done
+echo "heads " $heads
+echo "tails " $tails
